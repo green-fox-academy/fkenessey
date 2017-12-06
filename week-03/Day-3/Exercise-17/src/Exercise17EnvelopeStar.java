9805ae20f1ab3aeb3d41d68cb59373f48412da91
Franky <gfferike@gmail.com>
2017-12-06 16:15:38 +0100

    W3D3 - Exercise17

--- week-03/Day-3/Exercise-17/src/Exercise17EnvelopeStar.java
+++ week-03/Day-3/Exercise-17/src/Exercise17EnvelopeStar.java
@@ -4,7 +4,7 @@ import java.awt.*;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-public class Exercise16LinePlayQuarters {
+public class Exercise17EnvelopeStar {
   public static void mainDraw(Graphics graphics){
     // divide the canvas into 4 parts
     // and repeat this pattern in each quarter:
@@ -14,10 +14,10 @@ public class Exercise16LinePlayQuarters {
       drawLine(graphics, WIDTH/2, 0, WIDTH/2, WIDTH, Color.black);
       drawLine(graphics, 0, WIDTH/2, WIDTH, WIDTH/2, Color.black);
     }
-    drawLineForLoop(graphics, 0, 0, WIDTH/2, step);
-    drawLineForLoop(graphics, WIDTH/2, 0, WIDTH/2, step);
-    drawLineForLoop(graphics, 0, WIDTH/2, WIDTH/2, step);
-    drawLineForLoop(graphics, WIDTH/2, WIDTH/2, WIDTH/2, step);
+    drawLineForLoop(graphics, 0, 0, WIDTH/2, step, true, false);
+    drawLineForLoop(graphics, WIDTH/2, 0, WIDTH/2, step, false, false);
+    drawLineForLoop(graphics, 0, WIDTH/2, WIDTH/2, step, true, true);
+    drawLineForLoop(graphics, WIDTH/2, WIDTH/2, WIDTH/2, step, false, true);
   }
 
   public static void drawLine(Graphics graphics, int x1, int y1, int x2, int y2, Color color) {
@@ -25,10 +25,23 @@ public class Exercise16LinePlayQuarters {
     graphics.drawLine(x1, y1, x2, y2);
   }
 
-  public  static void drawLineForLoop(Graphics graphics, int x, int y, int width, int step) {
-    for (int j = 0; j <= width; j += step) {
-      drawLine(graphics, x, y + j, x + step + j, y + width, Color.green);
-      drawLine(graphics, x + j, y, x + width, y+ j + step, Color.magenta);
+  public  static void drawLineForLoop(Graphics graphics, int x, int y, int width, int step, boolean MirrorVertical, boolean MirrorHorizontal) {
+    if (!MirrorVertical && !MirrorHorizontal) {
+      for (int j = 0; j <= width; j += step) {
+        drawLine(graphics, x, y + j, x + step + j, y + width, Color.green);
+      }
+    } else if (MirrorVertical && !MirrorHorizontal){
+      for (int j = 0; j <= width; j += step) {
+        drawLine(graphics, x + j, y + width, x + width, y + width - j, Color.green);
+      }
+    } else if (!MirrorVertical && MirrorHorizontal) {
+      for (int j = 0; j <= width; j += step) {
+        drawLine(graphics, x, y + width - j, x + j, y, Color.green);
+      }
+    } else {
+      for (int j = 0; j <= width; j += step) {
+        drawLine(graphics, x + j, y, x + width, y + j, Color.green);
+      }
     }
   }
 
@@ -55,3 +68,4 @@ public class Exercise16LinePlayQuarters {
   }
 
 }
+
