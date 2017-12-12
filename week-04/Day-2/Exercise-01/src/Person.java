fbbd315f54a588239d195a0c1807059b50061f25
Franky <gfferike@gmail.com>
2017-12-12 11:23:34 +0100

    W4D2 - Exercise-01 - Student part

--- week-04/Day-2/Exercise-01/src/Person.java
+++ week-04/Day-2/Exercise-01/src/Person.java
@@ -22,4 +22,28 @@ public class Person {
   public void getGoal() {
     System.out.println("My goal is: Live for the moment!");
   }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public String getGender() {
+    return gender;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public void setGender(String gender) {
+    this.gender = gender;
+  }
 }
---
+++ week-04/Day-2/Exercise-01/src/Student.java
@@ -0,0 +1,31 @@
+public class Student extends Person {
+  private String previousOrganization;
+  private int skippedDays;
+
+  public Student() {
+    setName("Jane Doe");
+    setAge(30);
+    setGender("female");
+    this.previousOrganization = "The School of Life";
+    this.skippedDays = 0;
+  }
+
+  public Student (String name, int age, String gender, String previousOrganization) {
+    setName(name);
+    setAge(age);
+    setGender(gender);
+    this.previousOrganization = previousOrganization;
+  }
+
+  public void getGoal() {
+    System.out.println("Be a junior software developer.");
+  }
+
+  public void introduce() {
+    System.out.println("Hi, I'm " + getName() + " a " + getAge() + " year old " + getGender() + " from " + previousOrganization + " who skipped " + skippedDays + " days from the course already.");
+  }
+
+  public void skipDays(int numberOfDays) {
+    this.skippedDays+=numberOfDays;
+  }
+}
