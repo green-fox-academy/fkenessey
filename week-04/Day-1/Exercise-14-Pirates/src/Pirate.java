c77201b1464ac88e0fb85ffb91f4ef0b0a4385f0
Franky <gfferike@gmail.com>
2017-12-12 10:11:15 +0100

    W4D1 - Exercise-14 Pirates part

---
+++ week-04/Day-1/Exercise-14-Pirates/src/Pirate.java
@@ -0,0 +1,56 @@
+public class Pirate {
+  int alcoholLevel;
+  int howsItGoing;
+  boolean alive;
+  boolean parrot;
+
+  public Pirate() {
+    this.alcoholLevel = 10;
+    this.howsItGoing = 0;
+    this.alive = true;
+    this.parrot = false;
+  }
+
+  public Pirate(int alcoholLevel) {
+    this.alcoholLevel = alcoholLevel;
+  }
+
+  public void drinkSomeRum() {
+    if (alive) {
+      this.alcoholLevel += 10;
+    } else {
+      System.out.println("he's dead");
+    }
+  }
+
+  public void howsItGoingMate() {
+    if (howsItGoing < 5 && alive) {
+      System.out.println("Pour me anudder!");
+      howsItGoing++;
+    } else if (alive){
+      System.out.println("Arghh, I'ma Pirate. How d'ya d'ink its goin?");
+    } else {
+      System.out.println("he's dead");
+    }
+  }
+
+  public void die() {
+    this.alive = false;
+    this.parrot = false;
+  }
+
+  public void brawl(Pirate pirate1, Pirate pirate2) {
+    int randomNumber = (int)(Math.random()*100);
+    if (randomNumber <30 && pirate1.alive && pirate2.alive) {
+            pirate1.die();
+            pirate2.parrot = true;
+    } else if (randomNumber >= 60 && pirate1.alive && pirate2.alive) {
+      pirate2.die();
+      pirate1.parrot = true;
+    } else if (pirate1.alive && pirate2.alive) {
+      System.out.println("Both pirates passed out!");
+    } else {
+      System.out.println("Cannot fight a dead pirate!");
+    }
+  }
+}
