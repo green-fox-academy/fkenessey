ebc14b92d94bf5786f432196737ddf6f0e3933a8
Franky <gfferike@gmail.com>
2017-12-13 10:11:17 +0100

    W4D3 - Exercise-01

---
+++ week-04/Day-2/Exercise-03/src/aircraft.java
@@ -0,0 +1,16 @@
+public class aircraft {
+  private int ammoLevel;
+  private int maxAmmo;
+  private int damageLevel;
+  private int baseDamage;
+
+  public aircraft() {
+  }
+
+  public aircraft(int ammoLevel, int maxAmmo, int damageLevel, int baseDamage) {
+    this.ammoLevel = ammoLevel;
+    this.maxAmmo = maxAmmo;
+    this.damageLevel = damageLevel;
+    this.baseDamage = baseDamage;
+  }
+}
