bc05480fd2dafd116e26996b9a8df9dcdd6a92d2
Franky <gfferike@gmail.com>
2018-01-02 15:23:25 +0100

    Comparable - ToDo

---
+++ week-06/Day-1/ComparableToDo/src/Thing.java
@@ -0,0 +1,43 @@
+public class Thing implements Comparable{
+  private String name;
+  private boolean completed;
+
+  public Thing(String name) {
+    this.name = name;
+  }
+
+  public void complete() {
+    this.completed = true;
+  }
+
+  @Override
+  public String toString() {
+    return (completed ? "[x] " : "[ ] ") + name;
+  }
+
+  @Override
+  public int compareTo(Object o) {
+    Thing thingToCompare = (Thing) o;
+    int output = 0;
+    if (this.completed && thingToCompare.completed) {
+      if (this.name.charAt(0) == thingToCompare.name.charAt(0)) {
+        output = 0;
+      } else if (this.name.charAt(0) > thingToCompare.name.charAt(0)) {
+        output = 1;
+      } else {
+        output = -1;
+      }
+    } else if (this.completed) {
+      output = -1;
+    } else {
+      if (this.name.charAt(0) == thingToCompare.name.charAt(0)) {
+        output = 0;
+      } else if (this.name.charAt(0) > thingToCompare.name.charAt(0)) {
+        output = 1;
+      } else {
+        output = -1;
+      }
+    }
+    return output;
+  }
+}
