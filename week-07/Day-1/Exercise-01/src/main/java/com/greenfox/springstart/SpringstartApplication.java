3aeb5592e93ae349d29014e77993e5eb72dc013b
Franky <gfferike@gmail.com>
2018-01-08 14:56:06 +0100

    HelloWorld

--- week-07/Day-1/Exercise-01/src/main/java/com/greenfox/springstart/SpringstartApplication.java
+++ week-07/Day-1/Exercise-01/src/main/java/com/greenfox/springstart/SpringstartApplication.java
@@ -2,11 +2,16 @@ package com.greenfox.springstart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+
 public class SpringstartApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringstartApplication.class, args);
-	}
+  public static void main(String[] args) {
+      SpringApplication.run(SpringstartApplication.class, args);
+  }
 }
