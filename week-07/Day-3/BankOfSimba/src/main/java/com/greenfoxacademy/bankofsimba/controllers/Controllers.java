c1d26638a8308b9e779ade4031bc359f272965af
Franky <gfferike@gmail.com>
2018-01-10 13:31:38 +0100

    W7D3 - Bank of Simba - Filling multiple Accounts

--- week-07/Day-3/BankOfSimba/src/main/java/com/greenfoxacademy/bankofsimba/controllers/Controllers.java
+++ week-07/Day-3/BankOfSimba/src/main/java/com/greenfoxacademy/bankofsimba/controllers/Controllers.java
@@ -14,10 +14,28 @@ public class Controllers {
   public String show(Model model) {
 
     BankAccount bankAccount = new BankAccount("Simba", 2000, "lion");
+    model.equals(showInserter(model, bankAccount));
+
+    return "template";
+  }
+
+  @RequestMapping(value = "/texttester")
+  public String texttester(Model model) {
+
+    BankAccount bankAccount = new BankAccount("Simba", 2000, "lion");
+    model.equals(showInserter(model, bankAccount));
+
+    model.addAttribute("test1", "This is an <em>HTML</em> text. <b>Enjoy yourself!</b>");
+    model.addAttribute("test2", "This is an <em>HTML</em> text. <b>Enjoy yourself!</b>");
+
+    return "template";
+  }
+
+  public Model showInserter(Model model, BankAccount bankAccount){
     model.addAttribute("name", bankAccount.getName());
     model.addAttribute("balance", bankAccount.getBalance());
     model.addAttribute("animalType", bankAccount.getAnimalType());
 
-    return "template";
+    return model;
   }
 }
