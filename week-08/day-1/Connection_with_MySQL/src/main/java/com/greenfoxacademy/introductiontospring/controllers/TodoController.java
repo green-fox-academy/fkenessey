cda855f3788a7bae6f8f02b0622ecec6c3fca44a
Franky <gfferike@gmail.com>
2018-01-15 18:05:33 +0100

    Creating

--- week-08/day-1/Connection_with_MySQL/src/main/java/com/greenfoxacademy/introductiontospring/controllers/TodoController.java
+++ week-08/day-1/Connection_with_MySQL/src/main/java/com/greenfoxacademy/introductiontospring/controllers/TodoController.java
@@ -2,11 +2,12 @@ package com.greenfoxacademy.introductiontospring.controllers;
 
 import com.greenfoxacademy.introductiontospring.models.Todo;
 import com.greenfoxacademy.introductiontospring.repositories.ToDoRepository;
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,7 +20,6 @@ public class TodoController {
   ToDoRepository repo;
 
   @RequestMapping(value = {"/", "/list"})
-
   public String list(Model model, @RequestParam(value = "isActive", required = false) boolean isActive){
     List<Todo> repoList = new ArrayList<>();
     repo.findAll().forEach(repoList::add);
@@ -32,5 +32,18 @@ public class TodoController {
     }
     model.addAttribute("repoList", repoList);
     return "newtodo";
+  }
+
+ @GetMapping("/add")
+  public String add(Model model){
+    Todo todo = new Todo();
+    model.addAttribute("todo", todo);
+    return "view";
+  }
+
+  @PostMapping("/add")
+  public ModelAndView add(Model model, @ModelAttribute Todo todo){
+    repo.save(todo);
+    return new ModelAndView("redirect:/todo/");
   }
 }
