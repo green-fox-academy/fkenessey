aaf3b816ac770a4f471e03c65c1eaec4fd3e06bc
Franky <gfferike@gmail.com>
2018-02-16 09:12:55 +0100

    cleaning untracked files

---
+++ week-11/day-3/web-security-1/src/main/java/com/greenfoxacademy/todowithwebsecurity/TodowithwebsecurityApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.todowithwebsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodowithwebsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodowithwebsecurityApplication.class, args);
+	}
+}
